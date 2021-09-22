d807afc6150da24c90299c9afaa3aeba36f67980
islomnumanov <islom4ik.numanov@gmail.com>
2021-09-22 12:43:36 +0900

    m

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,22 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  state = { term: "alksfns" };
+  state = { term: "" };
 
-  onInputChange = () => {};
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = (event) => {
+    event.preventDefault(); // this prevents the default behavior of onSubmit that makes our browser page to rerender as soon as we press enter/submit the form
+
+    // Todo: make sure we call the callback from the parent component
+  };
 
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Search for Videos</label>
             <input
